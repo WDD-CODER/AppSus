5de15292e25214072e868acad97987918c335f05
Lioz Fired <friedlioz@gmail.com>
2025-07-02 21:03:07 +0300

    Display details

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -26,7 +26,26 @@ export function MailDetails() {
             .finally(() => setIsLoading(false))
     }
 
+    function onBack() {
+        navigate('/mail')
+    }
+
+    if (isLoading) return <div>Loading...</div>
+
+    const { subject, body, isRead, sentAt, from } = mail
     return (
-        <h1>its working</h1>
+        <section className="mail-details">
+            {/* will be here nav-header */}
+            <p className="mail-details-subject">{subject}</p>
+            <div className="mail-details-header">
+                <p className="mail-details-from">{from}</p>
+                <div className="function-btns">
+                    <p className="full-time-details">{sentAt}</p>
+                    <button className="starred-btn">⭐</button> {/* will change the icons */}
+                    <button className="replay-btn">↱</button>
+                </div>
+                <pre className="mail-details-body">{body}</pre>
+            </div>
+        </section>
     )
 }
