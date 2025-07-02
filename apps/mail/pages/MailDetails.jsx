ca7c7e18ad9f8bc9468d4d54e383b6e658d34660
Lioz Fired <friedlioz@gmail.com>
2025-07-02 18:24:48 +0300

    Work on mail details

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,9 +1,30 @@
 import { mailService } from "../services/mail.service.js"
+import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js"
 
 const { useParams, useNavigate, Link } = ReactRouterDOM
 const { useState, useEffect } = React
 
 export function MailDetails() {
+
+    const [mail, setMail] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const params = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        loadMail()
+    }, [params.mailId])
+
+    function loadMail() {
+        setIsLoading(true)
+        mailService.get(params.mailId)
+            .then(mail => setMail(mail))
+            .catch(err => {
+                console.log('err:', err)
+                showErrorMsg('Cannot get mail..')
+            })
+            .finally(() => setIsLoading(false))
+    }
 
     return (
         <h1>its working</h1>
