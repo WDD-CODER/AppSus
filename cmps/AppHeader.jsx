3378fa3bc20ba263f013a8d169676449073fedd7
Your Name <dan.weibren@gmail.com>
2025-07-05 20:03:58 +0300

    Refactored Mutual layouts.
     Left comments so we can make a Shared decision.

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -6,9 +6,9 @@ export function AppHeader() {
         <img className="hamburger" src="./assets/images/hamburger.png" alt="hamburger-img" />
         <img src="./assets/images/logo.png" alt="logo-img" />
 
-        <label htmlFor="search-bar container">
+        {/* <label htmlFor="search-bar container">
             <input type="text" id="search-bar" />
-        </label>
+        </label> */}
         <nav className="header-nav-bar flex align-center">
             <NavLink to="/mail"> <img src="./assets/images/mail.png" alt="mail-img" /><span className="hide-txt" >Mail</span></NavLink>
             <NavLink to="/note"> <img src="./assets/images/keep.png" alt="keep-img" /><span className="hide-txt">Keep</span></NavLink>
