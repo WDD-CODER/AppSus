5ff54236b5c710385407d8eeeae876812f4e186e
Lioz Fired <friedlioz@gmail.com>
2025-07-07 18:37:28 +0300

    Create new div

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,8 +4,26 @@ const { Fragment } = React
 
 export function MailPreview({ mail }) {
 
-    const { from, subject, body, sentAt } = mail
+    const { from, subject, body, sentAt, isRead } = mail
     const formatedTime = utilService.formatTimeOrDate(sentAt)
+    const checkIsMailRead = isReadMail()
+
+    function isReadMail() {
+        const readMail = {
+            iconClassName: '',
+            iconString: ''
+        }
+
+        if (isRead) {
+            readMail.iconClassName = 'icon-mark_email_unread icon'
+            readMail.iconString = 'mark_email_unread'
+        } else {
+            readMail.iconClassName = 'icon-drafts icon'
+            readMail.iconString = 'drafts'
+        }
+        return readMail
+    }
+
     return (
         <Fragment>
             <div className="mail-prev mail-from">
@@ -18,6 +36,10 @@ export function MailPreview({ mail }) {
             <div className="mail-prev mail-date">
                 <p>{formatedTime}</p>
             </div>
-        </Fragment>
+            <div className="mail-prev mail-act-btns">
+                <button className="icon-delete icon">delete</button>
+                <button className={checkIsMailRead.iconClassName}>{checkIsMailRead.iconString}</button>
+            </div>
+        </Fragment >
     )
 }
