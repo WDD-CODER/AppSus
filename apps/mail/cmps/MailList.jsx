ef4a17073f3002ac7e9c6c4c01efa75a153b7ebb
Lioz Fired <friedlioz@gmail.com>
2025-07-08 22:13:21 +0300

    Implement click mail to mail is read

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,16 +1,27 @@
+import { mailService } from "../services/mail.service.js"
 import { MailPreview } from "./MailPreview.jsx"
 import { MailPreviewBtns } from "./MailPreviewBtns.jsx"
 const { Link } = ReactRouterDOM
 
 export function MailList({ mails }) {
 
+    function readMail(mailToUpdate) {
+            const updatedMail = { ...mailToUpdate, isRead: true }
+    
+            mailService.save(updatedMail)
+                .then(savedMail => console.log(savedMail))
+                .catch(err => {
+                    console.log('Error updating mail read status:', err)
+                    showErrorMsg('Could not update mail status.')
+                })
+        }
 
     if (!mails.length) return <div>No Mails to Show...</div>
     return (
         <ul className="mail-list">
             {mails.map(mail => (
                 <li className={mail.isRead ? 'read' : 'unread'} key={mail.id}>
-                    <Link to={`/mail/${mail.id}`}>
+                    <Link to={`/mail/${mail.id}`} onClick={(ev) => readMail(mail)}>
                         <MailPreview mail={mail} />
                         <MailPreviewBtns mail={mail} />
                     </Link>
