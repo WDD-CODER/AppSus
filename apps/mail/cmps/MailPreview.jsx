42be7bf3483efbfa3f5dac4543d62995761e90cd
Lioz Fired <friedlioz@gmail.com>
2025-07-08 17:14:42 +0300

    Add 2 more buttons

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -4,12 +4,25 @@ const { Fragment } = React
 
 export function MailPreview({ mail }) {
 
+    const handleActionClick = (ev) => {
+        ev.preventDefault()
+        ev.stopPropagation()
+        console.log('Action button clicked for mail:', mail.id)
+
+        if (ev.target.dataset.action === 'check-box') console.log('check-box')
+        if (ev.target.dataset.action === 'starred') console.log('starred')
+    }
+
     const { from, subject, body, sentAt, isRead } = mail
     const formatedTime = utilService.formatTimeOrDate(sentAt)
 
     return (
         <Fragment>
             <div className="mail-prev mail-from">
+                <button className="icon-check_box_outline_blank icon"
+                    data-action="check-box" onClick={handleActionClick}>check_box_outline_blank</button>
+                <button className="icon-star icon"
+                    data-action="starred" onClick={handleActionClick}>star</button>
                 <p>{from}</p>
             </div>
             <div className="mail-prev mail-content">
