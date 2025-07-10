eda78001d86c8e5cbaaa9643488428117be7850d
Your Name <dan.weibren@gmail.com>
2025-07-10 08:25:23 +0300

    Adjusted. Combined up header. Removed unneeded images and relevant content.

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -2,14 +2,14 @@ const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
 
-    return <header className="app-header box">
-        <img className="hamburger" src="./assets/images/hamburger.png" alt="hamburger-img" />
+    return <header className="app-header">
+        {/* <img className="hamburger" src="./assets/images/hamburger.png" alt="hamburger-img" /> */}
         <img src="./assets/images/logo.png" alt="logo-img" />
 
         {/* <label htmlFor="search-bar container">
             <input type="text" id="search-bar" />
         </label> */}
-        <nav className="header-nav-bar flex align-center">
+        <nav className="header-nav-bar flex ">
             <NavLink to="/mail"> <img src="./assets/images/mail.png" alt="mail-img" /><span className="hide-txt" >Mail</span></NavLink>
             <NavLink to="/note"> <img src="./assets/images/keep.png" alt="keep-img" /><span className="hide-txt">Keep</span></NavLink>
             <NavLink to="/about">About</NavLink>
