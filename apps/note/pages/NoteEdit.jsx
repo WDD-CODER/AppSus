ffc163c262b633d4df641dadc94727352d6902fe
Your Name <dan.weibren@gmail.com>
2025-07-11 13:56:40 +0300

    Changed. Auto grow. Condition on edit note.
    Made more specific the text area definitions

--- apps/note/pages/NoteEdit.jsx
+++ apps/note/pages/NoteEdit.jsx
@@ -15,7 +15,9 @@ export function NoteEdit({ onCloseModal, selectedNote, onDeleteNote }) {
 
 
     useEffect(() => {
-        requestAnimationFrame(() => {if (textRef.current) autoGrow(textRef.current)})
+        
+        requestAnimationFrame(() => { if (textRef.current) debounce(autoGrow(textRef.current),5000 )})
+
         if (!note) {
             animateCSS(loadingRef.current, 'heartBeat', false)
             const note = noteService.getEmptyNote()
@@ -27,6 +29,7 @@ export function NoteEdit({ onCloseModal, selectedNote, onDeleteNote }) {
                     showSuccessMsg('saved to storage, ready for edit :)')
                 })
         }
+
     }, [searchParams.get('time-createdAt')])
 
 
@@ -35,6 +38,10 @@ export function NoteEdit({ onCloseModal, selectedNote, onDeleteNote }) {
     }, [note])
 
 
+
+    function seIsPinned() {
+
+    }
 
 
     function handleChange({ target }) {
@@ -70,62 +77,60 @@ export function NoteEdit({ onCloseModal, selectedNote, onDeleteNote }) {
 
     // const coverImg = (!note.style) ? {} : { ...note.style }
     if (!note) return (<div ref={loadingRef} className="loading"> Loading...</div>)
-    const transparentDrop =  (searchParams.get('time-createdAt')) ? false : true  
-        return (
-            <React.Fragment>
-                {transparentDrop && <div className="transparent-drop" onClick={() => onSave()}></div>}
-                <form className="note-edit-container box"
-                    onClick={(ev) => ev.stopPropagation()}
-                    style={{ ...note.style }}
-                    onSubmit={ev => {
-                        ev.preventDefault()
-                        onSave()
-                    }}>
+    const transparentDrop = (searchParams.get('time-createdAt')) ? false : true
+    return (
+        <React.Fragment>
+            {transparentDrop && <div className="transparent-drop" onClick={() => onSave()}></div>}
+            <form className="note-edit-container box"
+                onClick={(ev) => ev.stopPropagation()}
+                style={{ ...note.style }}
+                onSubmit={ev => {
+                    ev.preventDefault()
+                    onSave()
+                }}>
 
-                    <div className="text-info-container">
-                        {/* <h1 ></h1> */}
-                        <input className="title" onChange={handleChange}
-                            value={note.title || ''}
-                            name="title" type="text"
-                            placeholder="Title..." />
+                <div className="text-info-container">
+                    {/* <h1 ></h1> */}
+                    <input className="title" onChange={handleChange}
+                        value={note.title || ''}
+                        name="title" type="text"
+                        placeholder="Title..." />
 
 
-                        <label className="info" htmlFor="info">
-                            <textarea ref={textRef} onChange={ev => {
-                                autoGrow(ev.target)
-                                handleChange(ev)
-                            }}
-                                id="info"
-                                value={note.info.txt}
-                                name="info"
-                                type="text"
-                                placeholder="Take a note..." />
-                        </label>
+                    <label className="info" htmlFor="info">
+                        <textarea ref={textRef} onChange={ev => {
+                            autoGrow(ev.target)
+                            handleChange(ev)
+                        }}
+                            id="info"
+                            value={note.info.txt}
+                            name="info"
+                            type="text"
+                            placeholder="Take a note..." />
+                    </label>
 
-                    </div>
-                    {/* <div className="labels-container">{ Note.label && <LabelPicker/>}</div> */}
-                    <section className="tool-bar"><ToolBar onSetNote={setNote}>
-                        <button className="delete btn "
-                            data-type={'Delete'}
-                            onClick={ev => {
-                                ev.preventDefault()
-                                onRemoveNote()
-                            }}> <span className="icon-delete icon">delete</span>
-                        </button>
-                        {/* <button className="close btn "> */}
-                        <button
-                            onClick={ev => {
-                                ev.preventDefault()
-                                onSave()
-                            }} className="close">close</button>
-                        {/* </button> */}
-                    </ToolBar>
-                        <button className="pin-note">
-                            <span className=" icon-keep icon ">keep</span>
-                        </button>
+                </div>
+                {/* <div className="labels-container">{ Note.label && <LabelPicker/>}</div> */}
+                <section className="tool-bar"><ToolBar onSetNote={setNote}>
+                    <button className="delete btn "
+                        data-type={'Delete'}
+                        onClick={ev => {
+                            ev.preventDefault()
+                            onRemoveNote()
+                        }}> <span className="icon-delete icon">delete</span>
+                    </button>
+                    <button
+                        onClick={ev => {
+                            ev.preventDefault()
+                            onSave()
+                        }} className="close">close</button>
+                </ToolBar>
+                    <button className="pin-note" onClick={() => seIsPinned()}>
+                        <span className=" icon-keep icon ">keep</span>
+                    </button>
 
-                    </section>
-                </form>
-            </React.Fragment>
-        )
-    }
+                </section>
+            </form>
+        </React.Fragment>
+    )
+}
