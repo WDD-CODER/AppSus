f9659dba6c473aa0da1803d7467c94c42c907f99
Lioz Fired <friedlioz@gmail.com>
2025-07-11 14:50:25 +0300

    Implement delete from mail details

--- apps/mail/cmps/MailDetailsHeader.jsx
+++ apps/mail/cmps/MailDetailsHeader.jsx
@@ -1,4 +1,5 @@
 import { mailService } from "../services/mail.service.js";
+import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js";
 
 // const { useState, useEffect } = React
 
@@ -21,6 +22,40 @@ export function MailDetailsHeader({ onBack, mail, onToggleMailRead }) {
         return readMail
     }
 
+    function onDeleteMail(mailId) {
+        mailService.get(mailId)
+            .then(mail => {
+                if (!mail.removedAt) {
+                    mail.removedAt = Date.now()
+                    mailService.save(mail)
+                        .then(trashedMail => {
+                            console.log('mail has trashed!', trashedMail)
+                            showSuccessMsg('Mail is moved to trash folder..')
+                            onBack()
+                        })
+                        .catch(err => {
+                            console.log('err', err)
+                            showErrorMsg('cant trashed mail')
+                        })
+                } else {
+                    mailService.remove(mailId)
+                        .then(deletedMail => {
+                            console.log('mail has deleted!', deletedMail)
+                            showSuccessMsg('Mail has deleted!')
+                            onBack()
+                        })
+                        .catch(err => {
+                            console.log('err', err)
+                            showErrorMsg('Cant deleted mail')
+                        })
+                }
+            })
+            .catch(err => {
+                console.log('err', err)
+                showErrorMsg('Cant get mail..')
+            })
+    }
+
     const checkIsMailRead = isReadMail()
 
     function onMarkRead(ev) {
@@ -35,7 +70,7 @@ export function MailDetailsHeader({ onBack, mail, onToggleMailRead }) {
     return (
         <section className="mail-details-header">
             <button className="icon-arrow_back icon" onClick={onBack}>arrow_back</button>
-            <button className="icon-delete icon">delete</button>
+            <button className="icon-delete icon" onClick={(ev) => onDeleteMail(mail.id)}>delete</button>
             <button className={checkIsMailRead.iconClassName} data-action="mark-read"
                 onClick={onMarkRead}
             >{checkIsMailRead.iconString}</button>
