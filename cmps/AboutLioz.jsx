ff6ad0d8fcee0da3480e2e82bff2f21f692ca728
Lioz Fired <friedlioz@gmail.com>
2025-07-12 22:06:35 +0300

    Add img and few words

--- cmps/AboutLioz.jsx
+++ cmps/AboutLioz.jsx
@@ -7,7 +7,9 @@ export function AboutLioz() {
             <p>I’m Lioz — I’ve spent the last several years working in my family’s cooling systems business, building and maintaining industrial tech.
                  Lately, I’ve been looking for something new, something I can build with my brain more than my hands.
                  That’s what brought me to coding — a different kind of logic, same satisfaction when things finally click.
+                 I'm married to Chen +2 adorable kids.
             </p>
+            <img src="assets/images/lioz.jpg" alt="lioz-img" />
         </section>
 
     )
