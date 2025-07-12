a7e6a266afea970f2c3fa2b4af971f4e92552727
Lioz Fired <friedlioz@gmail.com>
2025-07-12 22:09:00 +0300

    Add surname

--- cmps/AboutLioz.jsx
+++ cmps/AboutLioz.jsx
@@ -3,8 +3,8 @@ export function AboutLioz() {
 
     return (
         <section className="about-lioz ">
-            <h2>About Lioz</h2>
-            <p>I’m Lioz — I’ve spent the last several years working in my family’s cooling systems business, building and maintaining industrial tech.
+            <h2>About Lioz Fried</h2>
+            <p>I’m Lioz Fried — I’ve spent the last several years working in my family’s cooling systems business, building and maintaining industrial tech.
                  Lately, I’ve been looking for something new, something I can build with my brain more than my hands.
                  That’s what brought me to coding — a different kind of logic, same satisfaction when things finally click.
                  I'm married to Chen +2 adorable kids.
